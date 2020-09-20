f69c2a240925aee6320c9d0ec405e1ce6df1eade
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-20 03:19:56 -0700

    The template

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,23 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
-  customer = new Customer();
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.buildForm(new Customer());
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  buildForm(defaults?: Customer): FormGroup {
+    const f = this.fb.group({
+      firstName: [
+        defaults.firstName,
+        [Validators.required, Validators.minLength(3)],
+      ],
+      lastName: [
+        defaults.lastName,
+        [Validators.required, Validators.maxLength(50)],
+      ],
+      email: [defaults.email, Validators.required],
+      sendCatalog: [defaults.sendCatalog],
+      address: this.buildAddressGroup(),
+    });
+    return f;
+  }
+
+  buildAddressGroup(): FormGroup {
+    const afg = this.fb.group({
+      addressType: ['home', Validators.required],
+      street1: ['', Validators.required],
+      street2: [''],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+    });
+    return afg;
+  }
+
+  save(values): void {
+    // form value model doesnt correspond to customer, you can map it to it manually
+    // but the course changes the model later so don't bother yet
+    console.log(values);
+    console.log('Saved: ' + JSON.stringify(values));
   }
 }
